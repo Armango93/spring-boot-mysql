fe5d3abebbb454f5d0f6075f0079d66214595f3d
Andrew Nikolaev <nikolaeff.expert@gmail.com>
2018-07-10 19:49:47 +0700

    Connected to Heroku with MySQL database

--- src/main/java/com/javasampleapproach/spring/mysql/model/Book.java
+++ src/main/java/com/javasampleapproach/spring/mysql/model/Book.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "book")
+@Table(name = "books")
 public class Book implements Serializable {
 
 	private static final long serialVersionUID = 1L;
